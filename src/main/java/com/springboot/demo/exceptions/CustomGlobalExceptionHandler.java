d195038011890415de78fdf60ce1b38212adcc2d
Ajith <ajithks797@gmail.com>
2022-03-25 09:37:03 +0530

    Http request method not supported exception handling

--- src/main/java/com/springboot/demo/exceptions/CustomGlobalExceptionHandler.java
+++ src/main/java/com/springboot/demo/exceptions/CustomGlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -20,5 +21,13 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
         CustomErrorDetails customErrorDetails = new CustomErrorDetails(new Date(),
                 "Method argument not valid exception thrown", ex.getMessage());
         return new ResponseEntity<>(customErrorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+            HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        CustomErrorDetails customErrorDetails = new CustomErrorDetails(new Date(),
+                "Http request method not supported exception thrown- method not allowed", ex.getMessage());
+        return new ResponseEntity<>(customErrorDetails, HttpStatus.METHOD_NOT_ALLOWED);
     }
 }
